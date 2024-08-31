912c496d0bd5b7604d3ff707a0de1ac5164c35fe
SoieYoo <yoosoie0000@gmail.com>
2024-08-31 15:54:09 +0900

    first commit

---
+++ src/main/java/com/partimestudy/PartimestudyApplication.java
@@ -0,0 +1,13 @@
+package com.partimestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartimestudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartimestudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/partimestudy/PartimestudyApplicationTests.java
@@ -0,0 +1,15 @@
+package com.partimestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class PartimestudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
